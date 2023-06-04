4804ddf3b20bee520cf7da7c26eea42252af5b9e
mahiiram <mahendran.ramar9797@gmail.com>
2023-06-04 18:33:03 +0530

    new commit

--- src/Home.js
+++ src/Home.js
@@ -2,8 +2,9 @@ import React from 'react'
 
 function Home() {
   return (
-    <div>
+    <div style={{display:'flex',flexDirection:'column',justifyContent:'center',margin:"auto"}}>
     <h1 className='welcome' style={{color: "black"}}>Welcome to Student Teacher Application</h1>
+    <img style={{height:'500px'}} src="https://www.edsys.in/wp-content/uploads/How_blended_learning_can_improve_student-teacher_relations.png" aly="student-teacher" />
     </div>
   )
 }
